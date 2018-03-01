75ed2b0e0039871ea8391e0979ada69eba507fd2
Icer <liangshuang@uchaindb.com>
2018-03-01 08:36:04 +0800

    properly show loading/error/noinfo message to user when requesting chain info

--- src/ClientApp/app/components/database/chain.page.ts
+++ src/ClientApp/app/components/database/chain.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input, isDevMode } from '@angular/core';
+import { Location } from '@angular/common';
 import { ParamMap, ActivatedRoute, Router } from '@angular/router';
 import { ChainDbService } from '../../services/chain-db.service';
 import { ChainDb, Transaction, Block } from '../../models/chain-db.model';
@@ -9,12 +10,16 @@ import { ChainDb, Transaction, Block } from '../../models/chain-db.model';
     styleUrls: ['./common.css']
 })
 export class DatabaseChainPage implements OnInit {
+    dbid: string;
+    mixid: string;
     db: ChainDb;
-    result: any;
     block: Block;
     transaction: Transaction;
+    error = false;
+    loading = true;
 
     constructor(
+        private location: Location,
         private route: ActivatedRoute,
         private router: Router,
         private dataService: ChainDbService
@@ -24,21 +29,40 @@ export class DatabaseChainPage implements OnInit {
     ngOnInit() {
         this.route.paramMap
             .subscribe((params: ParamMap) => {
-                let dbid = params.get('dbid');
-                let mixid = params.get('id');
-                this.dataService.getChainDb(dbid)
+                this.dbid = params.get('dbid');
+                this.mixid = params.get('id');
+                this.dataService.getChainDb(this.dbid)
                     .subscribe(_ => {
                         this.db = _;
-                        this.dataService.getQueryChain(this.db, mixid)
-                            .subscribe(_ => {
-                                this.result = _;
-                                this.block = _.Block;
-                                this.transaction = _.Transaction;
-                            });
+                        this.loadData();
                     });
+            });
+    }
+
+    loadData() {
+        this.error = false;
+        this.loading = true;
+        setTimeout(() => {
+            if (this.loading) {
+                this.block = null;
+                this.transaction = null;
+            }
+        }, 500);
+        this.dataService.getQueryChain(this.db, this.mixid)
+            .subscribe(_ => {
+                this.block = _.Block;
+                this.transaction = _.Transaction;
+                this.loading = false;
             },
-            err => isDevMode() && console.error(err)
-            );
+            err => {
+                isDevMode() && console.error(err);
+                this.error = true;
+                this.loading = false;
+            });
+    }
+
+    back() {
+        this.location.back()
     }
 
 }
